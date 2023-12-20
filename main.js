76e26d953a0b990ad5b518a1aa5bf35c04af22a9
lroux07 <lroux@edenschool.fr>
2023-12-20 16:25:00 +0100

    Finish layout menu

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import './style.css';
 
+
+// Gameplay
+
 const cursor = document.querySelector("#cursor");
 
 let moveLeft = 80;
@@ -27,5 +30,53 @@ addEventListener("keydown", function(e) {
       moveDown += 60
       cursor.style.marginTop = moveDown + "px";
       break;
+  }
+});
+
+
+// Menu
+
+const menu = document.querySelector("#menu");
+
+const btn_moins = document.querySelector("#btn_moins");
+const p_counter = document.querySelector("#counter");
+let counter = 10
+const btn_plus = document.querySelector("#btn_plus");
+const validate_btn = document.querySelector("#validate_btn");
+
+// Moins
+
+btn_moins.addEventListener("click", ()=> {
+  counter--
+  p_counter.textContent = counter
+
+  if(counter <= 1) {
+    btn_moins.disabled = true
+  } else if(counter <= 15) {
+    btn_plus.disabled = false
+  }
+})
+
+
+// Plus
+
+btn_plus.addEventListener("click", ()=> {
+  counter++
+  p_counter.textContent = counter
+
+  if(counter >= 15) {
+    btn_plus.disabled = true
+  } else if(counter > 1) {
+    btn_moins.disabled = false
+  }
+})
+
+validate_btn.addEventListener("click", ()=> {
+  menu.style.display = "none";
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape') {
+    menu.style.display = "flex";
   }
 });
