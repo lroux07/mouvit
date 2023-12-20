c9bbb99c7608026ac60a43ffb032933d394495f3
lroux07 <lroux@edenschool.fr>
2023-12-20 10:27:56 +0100

    assign keys

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-import './style.css'
+import './style.css';
 
 const cursor = document.querySelector("#cursor");
+
+document.onkeydown = function(e) {
+  switch (e.code) {
+    case "ArrowLeft":
+      cursor.style.position = "relative";
+      cursor.style.right = "80px";
+      break;
+    case "ArrowUp":
+      cursor.style.position = "relative";
+      cursor.style.bottom = "100px"
+      break;
+    case "ArrowRight":
+      cursor.style.marginLeft = "80px";
+      break;
+    case "ArrowDown":
+      cursor.style.marginTop = "100px";
+      break;
+  }
+}
